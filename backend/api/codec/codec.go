36ddc6f09361dad3061fc7530eaafae3f95d400f
detj <detj@users.noreply.github.com>
2025-03-14 17:18:13 +0530

    fix(backend): record and ingest errors for ios
    - record was writing corrupted dsym.tgz file to fs
    - fix the way scan would read builds during scanning
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- backend/api/codec/codec.go
+++ backend/api/codec/codec.go
@@ -13,8 +13,28 @@ import (
 // IsTarGz validates that the expected file
 // is a valid gzipped tarball.
 func IsTarGz(file io.Reader) (err error) {
+	// copy the contents of file into a buffer
+	//
+	// copy is essential here because otherwise
+	// this function advances the file pointer
+	// which then results into corrupted data
+	// downstream.
+	//
+	// FIXME: implement a better check which
+	// either copies a few bytes just to read
+	// the magic number from the header or
+	// a different approach to not advance
+	// the file pointer and still be efficient.
+	var buf bytes.Buffer
+	if _, err = io.Copy(&buf, file); err != nil {
+		return
+	}
+
+	// create a new reader from the buffer
+	r := bytes.NewReader(buf.Bytes())
+
 	// check if gzip file
-	gzipReader, err := gzip.NewReader(file)
+	gzipReader, err := gzip.NewReader(r)
 	if err != nil {
 		if errors.Is(err, gzip.ErrHeader) {
 			err = errors.Join(errors.New("not a valid gzip file"), err)
